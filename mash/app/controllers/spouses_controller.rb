8fa17547b3c146b754e441a8d35a7c4f84c1ff88
Stephen Deddens <sdeddens@gmail.com>
2015-09-28 16:25:34 -0500

    added spouses erbs and conrtoller rb

--- mash/app/controllers/spouses_controller.rb
+++ mash/app/controllers/spouses_controller.rb
@@ -1,2 +1,47 @@
 class SpousesController < ApplicationController
+
+    def index
+        @spouse = Spouse.all
+    end
+
+    def show
+        @spouse = Spouse.find(params[:id])
+    end
+
+    def new
+        @spouse = Spouse.new
+    end
+
+    def create
+        @spouse = Spouse.new(spouse_params)
+
+        if @spouse.save
+            redirict_to @spouse
+        end
+    end
+
+    def edit
+        @spouse = Spouse.find(params[:id])
+    end
+
+    def udpate
+        @spouse = Spouse.find(params[:id])
+
+        if @spouse.update(spouse_params)
+            redirect_to @spouse
+        end
+    end
+
+    def destroy
+        @spouse = Spouse.find(params[:id])
+        @spouse.destroy
+
+        redirect_to '/spouses'
+    end
+
+    private
+        def spouse_params
+            params.require(:spouse).permit(:post)
+        end
+
 end
