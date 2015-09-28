00befe6ff612c38ef2c3480f56b0b81ae70c5d33
Erich Abbott <abbotterich@gmail.com>
2015-09-28 14:49:53 -0500

    views indes car model

--- mash/db/migrate/20150928184528_create_cars.rb
+++ mash/db/migrate/20150928184528_create_cars.rb
@@ -1,6 +1,10 @@
 class CreateCars < ActiveRecord::Migration
   def change
     create_table :cars do |t|
+    	t.string :make 
+    	t.string :model
+    	t.string :year
+    	t.string :theme
 
       t.timestamps null: false
     end
