7ee8c1f39096919a234a3c5daebf83b4f40f9c32
Erich Abbott <abbotterich@gmail.com>
2015-09-28 16:39:55 -0500

    cars view controller model

--- mash/app/controllers/cars_controller.rb
+++ mash/app/controllers/cars_controller.rb
@@ -1,2 +1,23 @@
 class CarsController < ApplicationController
+	before_action
+
+	def index
+
+	end
+
+	def show
+	end
+
+	def create
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def destroy 
+		
+	end
 end
