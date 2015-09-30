82c4b7b4726518b388cdc8bc0ae4db27aaf7ec63
Larissa Tater <larissa.tater@gmail.com>
2015-09-30 12:19:37 -0500

    stupid github

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,6 +8,7 @@ class GamesController < ApplicationController
 		@houses = House.show_houses 2
 		@spouses = Spouse.show_spouses 2
 		@gameboard = {Cars: @cars}, {Houses: @houses}, {Spouses: @spouses}
+		
 
 	end
 end
