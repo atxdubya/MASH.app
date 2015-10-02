ecfc2bc89b64a6373e684a9d677a7f144b822b3a
Larissa Tater <larissa.tater@gmail.com>
2015-10-02 10:41:24 -0500

    Steve's game logic incorporated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,31 @@
-users = User.create([
-	{first_name: 'Larissa', last_name: 'Tater', email: 'larissa@gmail.com', password: 'lt-password'},
-	{first_name: 'Erich', last_name: 'Abbott', email: 'erich@gmail.com', password: 'ea-password'},
-	{first_name: 'Nicholas', last_name: 'Wallace', email: 'nicholas@gmail.com', password: 'nw-password'},
-	{first_name: 'Stephen', last_name: 'Deddens', email: 'stephen@gmail.com', password: 'sd-password'}])
+# users = User.create([
+# 	{first_name: 'Larissa', last_name: 'Tater', email: 'larissa@gmail.com', password: 'lt-password'},
+# 	{first_name: 'Erich', last_name: 'Abbott', email: 'erich@gmail.com', password: 'ea-password'},
+# 	{first_name: 'Nicholas', last_name: 'Wallace', email: 'nicholas@gmail.com', password: 'nw-password'},
+# 	{first_name: 'Stephen', last_name: 'Deddens', email: 'stephen@gmail.com', password: 'sd-password'}])
 
-houses = House.create([
-	{style: 'Mansion', theme: 'classic'},
-	{style: 'Apartment', theme: 'classic'},
-	{style: 'Shack', theme: 'classic'},
-	{style: 'House', theme: 'classic'}])
+# houses = House.create([
+# 	{style: 'Mansion', theme: 'classic'},
+# 	{style: 'Apartment', theme: 'classic'},
+# 	{style: 'Shack', theme: 'classic'},
+# 	{style: 'House', theme: 'classic'}])
 
-cars = Car.create([
-	{name: '2003 Ford Focus', theme: 'lame'},
-	{name: '2015 Toyota Tacoma', theme: 'practical'},
-	{name: '2000 Ford Ranger', theme: 'farm'},
-	{name: '2016 Honda Civic', theme: 'practical'}
-	])
+# cars = Car.create([
+# 	{name: '2003 Ford Focus', theme: 'lame'},
+# 	{name: '2015 Toyota Tacoma', theme: 'practical'},
+# 	{name: '2000 Ford Ranger', theme: 'farm'},
+# 	{name: '2016 Honda Civic', theme: 'practical'}
+# 	])
 
-spouses = Spouse.create([
-	{name: 'Madonna', gender: 'female', theme: 'celebrity'},
-	{name: 'Barack Obama', gender: 'male', theme: 'president'},
-	{name: 'Brad Pitt', gender: 'male', theme: 'celebrity'},
-	{name: 'Tom Hardy', gender: 'male', theme: 'British'}
+# spouses = Spouse.create([
+# 	{name: 'Madonna', gender: 'female', theme: 'celebrity'},
+# 	{name: 'Barack Obama', gender: 'male', theme: 'president'},
+# 	{name: 'Brad Pitt', gender: 'male', theme: 'celebrity'},
+# 	{name: 'Tom Hardy', gender: 'male', theme: 'British'}
+# 	])
+
+games = Game.create([
+	{house: 'Mansion'}, {spouse: 'Barack Obama'}, {car: '2003 Ford Focus'}, {references: 1}
 	])
 
 
