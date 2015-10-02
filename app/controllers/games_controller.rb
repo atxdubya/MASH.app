5c0482e162824a9a9d6cf943930d327e7c432789
Stephen Deddens <sdeddens@gmail.com>
2015-10-02 08:06:37 -0500

    updated games_controller and broken games/index.html.erb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,20 +4,20 @@ class GamesController < ApplicationController
 
 	def index
 
-		@cars = Car.show_cars 2
-		@houses = House.show_houses 2
-		@spouses = Spouse.show_spouses 2
+		@cars = Car.show_cars 4
+		@houses = House.show_houses 4
+		@spouses = Spouse.show_spouses 4
 
 	end
 
- 	def solveit
+ 	def solveit # I think this will auto redirect_to /solveit if no option given,  sgd
 
  		@plight = Game.solveit(params)
- 		# redirect_to '/games/'
- 		byebug
- 			render :index
+ 		# redirect_to '/games/'   #  option
+ 			render :index						#  option
 
  	end
 
 
 end
+
