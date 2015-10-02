1a7fb634e349900420bce031a2abfefc8134c50b
Stephen Deddens <sdeddens@gmail.com>
2015-10-01 21:24:09 -0500

    getting solution output to the game index view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,111 +10,14 @@ class GamesController < ApplicationController
 
 	end
 
+ 	def solveit
+
+ 		@plight = Game.solveit(params)
+ 		# redirect_to '/games/'
+ 		byebug
+ 			render :index
+
+ 	end
 
 
-	def solveit
-
-		# require 'pry'  (for debugging in the ruby rebel)
-		# binding.pry    (for setting a breakpoint when using the ruby rebel)
-		# byebug         (for setting breakpoint when in rails)
-
-		cycle = 6  #( roll of the dice )
-
-		# following are our test mashes:
-		# Note: We need to remove ["", nil], otherwise, just one nil or empty string
-		#   will cause a never ending loop! Also note: an empty string returns nil!
-
-		# abode  = ["Manson", "Apartment", "Shack",nil, "House"]-["", nil]
-		# wheels = ["Bus", "Bicycle", "Mercedes", "Geo", "Feet"]-["", nil]
-		# spouse = ["Julia", "Sponge Bob Square Pants", "Hillery", "Madonna", "Gilligan"]-["", nil]
-		# kids   = ['1','Twins', '3', 'Twin girls and twin boys!']-["", nil]
-
-#  params   This is a copy from byebug test on returned params hash
-# {"utf8"=>"✓",
-#  "authenticity_token"=>"KscWqG3yEAKTTSJELvpRSiy707aG2NABamNQcuT2HYuxikZ3o1qgh14nlEymvwxO2j3K0GyPQikw1hPqkpWblQ==",
-#  "cars"=>["Toyota", "Ford"],
-#  "spouses"=>["Brad Pitt", "Madonna"],
-#  "houses"=>["apartment", "shack"],
-#  "controller"=>"games",
-#  "action"=>"solveit"}
-
-		# Extract the game arrays from the params hash and if all the fields
-		#    on a section of the game board are empty, give them a default entry.
-		# Special note: mashes array must be modified to track each extracted array!
-		houses = params[:houses]-["", nil]
-		if houses.length == 0 then
-			houses = ["No house? Too bad, you're sleeping on the street."]
-		end
-
-		cars = params[:cars]-["", nil]
-		if cars.length == 0 then
-			cars = ["No car? Too bad, you're walking."]
-		end
-
-		spouses = params[:spouses]-["", nil]
-		if spouses.length == 0 then
-			spouses = ["No spouse? Too bad, you need a friend."]
-		end
-
-		# Load mashes with the arrays we want to use in the order we want to use them.
-		# See Special note above.
-		mashes = [houses, cars, spouses]
-
-		# Then build a store for our answers.
-		@plight = Array.new(mashes.length)
-
-		# given above, the challenge is to use cycle to get @plight
-		# method is to iterate over the arrays, testing for nil and testing cycle_ndx = cycle
-		# if not nil, update respective slot in @plight with value and
-		# set origin value to null, only increment index counter if origin not nil.
-		# when totalIncCounter = total entries in arrays... we are done.
-
-		# iterate over the mashes array to find the total entries
-
-		total_entries = houses.length + cars.length + spouses.length
-		# when we have time try to refactor the above with something like the following
-		# mashes.each {|x| total_entries += x.length}
-
-		cycle_counter = 1  # when get to cycle then we extract a @plight value
-		mash_ndx = 0  # keeps track of current mash
-		cell_ndx = 0  # keeps track of current cell in current mash
-		nil_counter = 0  # keeps track of total nils set. Determines end of game.
-
-		#check range
-		loop do  # loop over each successive array checking for nil.
-
-			# index remediation: if we walked out of current mash...
-			if cell_ndx == mashes[mash_ndx].length
-				cell_ndx = 0
-				mash_ndx += 1
-				if mash_ndx == mashes.length
-					mash_ndx = 0
-				end
-			end
-
-			if mashes[mash_ndx][cell_ndx] != nil then # we have something to do
-				if cycle_counter == cycle then # cell has data and is time to extract it
-					# move the value into the @plight,
-					@plight[mash_ndx] = mashes[mash_ndx][cell_ndx]
-
-					# then set value to nil,
-					mashes[mash_ndx][cell_ndx] = nil
-
-					nil_counter += 1
-
-					if nil_counter == total_entries then# we have the solution!
-
-						#return @plight
-						break
-					end
-					# reset the cycle_counter
-					cycle_counter = 1
-				else # cell has data but we skipped over it
-					cycle_counter += 1
-				end
-			end # go to next cell
-			cell_ndx += 1
-		end
-		render :solution
-	end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,81 +3,111 @@ class Game < ActiveRecord::Base
 	# belongs_to :user
 	# has_many :cars, :houses, :spouses
 
-# 	def self.solveit
-# 		# require 'pry'  (for debugging in the ruby rebel)
-# 		# binding.pry    (for setting a breakpoint when using the ruby rebel)
-# 		# byebug         (for setting breakpoint when in rails)
+	def self.solveit(params)
 
-# 		cycle = 6  #( roll of the dice )
+		# require 'pry'  (for debugging in the ruby rebel)
+		# binding.pry    (for setting a breakpoint when using the ruby rebel)
+		# byebug         (for setting breakpoint when in rails)
 
-# 		# following are our mashes:
-# 		# Note... on foreign code run .compact!
-# 		# Otherwise, just one nil will cause a never ending loop!
-# 		abode  = ["Manson", "Apartment", "Shack",nil, "House"].compact!
-# 		wheels = ["Bus", "Bicycle", "Mercedes", "Geo", "Feet"]
-# 		spouse = ["Julia", "Sponge Bob Square Pants", "Hillery", "Madonna", "Gilligan"]
-# 		# kids   = ['1','Twins', '3', 'Twin girls and twin boys!']
+		cycle = 6  #( roll of the dice )
 
-# 		# houses = [@houses].compact!
-# 		# cars = [@cars]
-# 		# spouses = [@spouses]
+		# following are our test mashes:
+		# Note: We need to remove ["", nil], otherwise, just one nil or empty string
+		#   will cause a never ending loop! Also note: an empty string returns nil!
 
-# 		mashes = [abode, wheels, spouse, kids]
+		# abode  = ["Manson", "Apartment", "Shack",nil, "House"]-["", nil]
+		# wheels = ["Bus", "Bicycle", "Mercedes", "Geo", "Feet"]-["", nil]
+		# spouse = ["Julia", "Sponge Bob Square Pants", "Hillery", "Madonna", "Gilligan"]-["", nil]
+		# kids   = ['1','Twins', '3', 'Twin girls and twin boys!']-["", nil]
 
-# 		# mashes = [houses, cars, spouses]
+#  params   This is a copy from byebug test on returned params hash
+# {"utf8"=>"✓",
+#  "authenticity_token"=>"KscWqG3yEAKTTSJELvpRSiy707aG2NABamNQcuT2HYuxikZ3o1qgh14nlEymvwxO2j3K0GyPQikw1hPqkpWblQ==",
+#  "cars"=>["Toyota", "Ford"],
+#  "spouses"=>["Brad Pitt", "Madonna"],
+#  "houses"=>["apartment", "shack"],
+#  "controller"=>"games",
+#  "action"=>"solveit"}
 
-# 		# plight = {abode:nil, wheels:nil, spouse:nil, kids:nil}    # this is a hash...
-# 		@plight = Array.new(mashes.length) # build a store for our answers here.
+		# Extract the game arrays from the params hash and if all the fields
+		#    on a section of the game board are empty, give them a default entry.
+		# Special note: mashes array must be modified to track each extracted array!
 
-# 		# given above, the challenge is to use cycle to get plight
-# 		# method is to iterate over the arrays, testing for nil and cycle_ndx = cycle
-# 		# if not nil, update respective slot in plight with value and
-# 		# set origin value to null, only increment index counter if origin not nil.
-# 		# total when totalIncCounter = total entries in arrays... we are done.
+		cars = params[:cars]-["", nil]
+		if cars.length == 0 then
+			cars = ["No car? Too bad, you're walking."]
+		end
 
-# 		#total_entries = abode.length + wheels.length + spouse.length + kids.length
-		
-# 		total_entries = houses.length + cars.length + spouses.length
+		spouses = params[:spouses]-["", nil]
+		if spouses.length == 0 then
+			spouses = ["No spouse? Too bad, you need a friend."]
+		end
 
-# 		cycle_counter = 1  # when get to cycle then we extract a plight value
-# 		mash_ndx = 0  # keeps track of current mash
-# 		cell_ndx = 0  # keeps track of current cell in current mash
-# 		nil_counter = 0  # keeps track of total nils set. Determines end of game.
+		houses = params[:houses]-["", nil]
+		if houses.length == 0 then
+			houses = ["No house? Too bad, you're sleeping on the street."]
+		end
 
-# 		#check range
-# 		loop do  # loop over each successive array checking for nil.
+		# Load mashes with the arrays we want to use in the order we want to use them.
+		# See Special note above.
+		mashes = [cars, spouses, houses]
 
-# 			# index remediation: if we walked out of current mash...
-# 			if cell_ndx == mashes[mash_ndx].length
-# 				cell_ndx = 0
-# 				mash_ndx += 1
-# 				if mash_ndx == mashes.length
-# 					mash_ndx = 0
-# 				end
-# 			end
+		# Then build a store for our answers.
+		@plight = Array.new(mashes.length)
 
-# 			if mashes[mash_ndx][cell_ndx] != nil then # we have something to do
-# 				if cycle_counter == cycle then # cell has data and is time to extract it
-# 					# move the value into the plight,
-# 					plight[mash_ndx] = mashes[mash_ndx][cell_ndx]
+		# given above, the challenge is to use cycle to get @plight
+		# method is to iterate over the arrays, testing for nil and testing cycle_ndx = cycle
+		# if not nil, update respective slot in @plight with value and
+		# set origin value to null, only increment index counter if origin not nil.
+		# when totalIncCounter = total entries in arrays... we are done.
 
-# 					# then set value to nil,
-# 					mashes[mash_ndx][cell_ndx] = nil
+		# iterate over the mashes array to find the total entries
 
-# 					nil_counter += 1
+		total_entries = houses.length + cars.length + spouses.length
+		# when we have time try to refactor the above with something like the following
+		# mashes.each {|x| total_entries += x.length}
 
-# 					if nil_counter == total_entries then# we have the solution!
-# 						#return plight
-# 						break
-# 					end
+		cycle_counter = 1  # when get to cycle then we extract a @plight value
+		mash_ndx = 0  # keeps track of current mash
+		cell_ndx = 0  # keeps track of current cell in current mash
+		nil_counter = 0  # keeps track of total nils set. Determines end of game.
 
-# 					# reset the cycle_counter
-# 					cycle_counter = 1
-# 				else # cell has data but we skipped over it
-# 					cycle_counter += 1
-# 				end
-# 			end # go to next cell
-# 			cell_ndx += 1
-# 		end
-# 	end
+		#check range
+		loop do  # loop over each successive array checking for nil.
+
+			# index remediation: if we walked out of current mash...
+			if cell_ndx == mashes[mash_ndx].length
+				cell_ndx = 0
+				mash_ndx += 1
+				if mash_ndx == mashes.length
+					mash_ndx = 0
+				end
+			end
+
+			if mashes[mash_ndx][cell_ndx] != nil then # we have something to do
+				if cycle_counter == cycle then # cell has data and is time to extract it
+					# move the value into the @plight,
+					@plight[mash_ndx] = mashes[mash_ndx][cell_ndx]
+
+					# then set value to nil,
+					mashes[mash_ndx][cell_ndx] = nil
+
+					nil_counter += 1
+
+					if nil_counter == total_entries then# we have the solution!
+
+						return @plight
+						# break      detritus from living on the controller
+
+					end
+					# reset the cycle_counter
+					cycle_counter = 1
+				else # cell has data but we skipped over it
+					cycle_counter += 1
+				end
+			end # go to next cell
+			cell_ndx += 1
+		end
+		# render :solution    detritus from living on the controller
+	end
  end
